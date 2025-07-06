4e68cff96a8f987260ed8758e5f90c649f605097
TreasureUzoma <treasureuzoma650@gmail.com>
2025-07-06 11:04:02 +0100

    final fix

---
+++ next.config.js
@@ -0,0 +1,32 @@
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "kairosapp.vercel.app",
+        pathname: "/**"
+      }
+    ]
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path+",
+        destination: "https://kairosapp.vercel.app/:path+"
+      },
+      {
+        source: "/app/_next/:path+",
+        destination: "https://kairosapp.vercel.app/app/_next/:path+"
+      }
+    ];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"] // Add @svgr/webpack to handle SVGs as React components
+    });
+    return config;
+  }
+};
+
+export default nextConfig;
